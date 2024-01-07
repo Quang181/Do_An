e756e0590d09291cf0d536abca66f952e5ec7396
Quang181 <m.quang.20011111@gmail.com>
2024-01-07 23:22:02 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from api.common_blue_print import app
+from flask_cors import CORS
+
+CORS(app)
+
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+
 
 if __name__ == '__main__':
     app = app
