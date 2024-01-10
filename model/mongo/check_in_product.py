1c2533e507f8cb7440d6a83d4d1877386ee689a3
Quang181 <m.quang.20011111@gmail.com>
2024-01-11 00:20:21 +0700

    update

---
+++ model/mongo/check_in_product.py
@@ -0,0 +1,20 @@
+from model.mongo.base_mongo_model import BaseMongo, CONFIG_ACCOUNT_DB
+
+
+class CheckInProduct(BaseMongo):
+    id_product = "id_product"
+    name = "name"
+    cccd = "cccd"
+    phone = "phone"
+    email = "email"
+    status = "status"
+    time_check_in = "time_check_in"
+    time_check_out = "time_check_out"
+
+    class Status:
+        check_in = "check_in"
+        check_out = "check_out"
+        pre_order = "pre_order"
+    def __init__(self, col=None):
+        super().__init__(col)
+        self.col = CONFIG_ACCOUNT_DB["check_in_product"]
